7db16bffb0c51e91afdb61863cf2f24a08e3be1b
Moon-Seokmin <msm0748@gmail.com>
2022-05-14 23:22:24 +0900

    version 0.1.0

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const request = require("request");
     height: 1080,
   });
 
-  const PROJECT_FOLDER_NAME = await page.evaluate(() => prompt("폴더명을 입력해주세요.", "양평정원"));
-  const URL = await page.evaluate(() => prompt("url을 입력해주세요.", "https://www.yp21.go.kr/ypjeongwon/index.do"));
+  const PROJECT_FOLDER_NAME = await page.evaluate(() => prompt("생성하실 폴더명을 입력해주세요.", ""));
+  const URL = await page.evaluate(() => prompt("소스를 가져올 url을 입력해주세요.", ""));
   await page.goto(URL);
 
   const mainHostUrl = await page.evaluate(() => {
@@ -24,6 +24,7 @@ const request = require("request");
   await createMainPage(page, PROJECT_FOLDER_NAME);
   await createScriptFile(page, PROJECT_FOLDER_NAME, mainHostUrl);
   await createCssFile(page, PROJECT_FOLDER_NAME, mainHostUrl);
+  console.log("파일 생성이 완료 되었습니다");
 })();
 
 async function createMainPage(page, PROJECT_FOLDER_NAME) {
@@ -38,27 +39,26 @@ async function createMainPage(page, PROJECT_FOLDER_NAME) {
 
   await createFolder(mainPagePath); // 프로젝트 폴더 생성
   await download(mainURL, `${mainPagePath}/index.html`, function () {
-    console.log("main page success");
+    console.log("main page 생성 성공");
   });
 }
 
 async function createScriptFile(page, PROJECT_FOLDER_NAME, mainHostUrl) {
   const scriptURLs = await page.$$eval("script", (links) =>
-    links.map((link) => {
-      if (link.src !== "") {
-        return link.src;
-      } else {
-        return false;
-      }
-    })
+    links.filter((link) => link.src !== "").map((link) => link.src)
   );
   await filteringDownload(page, PROJECT_FOLDER_NAME, mainHostUrl, scriptURLs);
-  // 스크립트 파일 생성;
   await page.waitForTimeout(1000);
 }
 
 async function filteringDownload(page, PROJECT_FOLDER_NAME, mainHostUrl, URLs) {
-  for await (const URL of URLs) {
+  for (let URL of URLs) {
+    if (!URL.includes(mainHostUrl)) {
+      continue;
+    }
+    if (URL.includes("?")) {
+      URL = URL.slice(0, URL.lastIndexOf("?"));
+    }
     let forderName = URL.replace(mainHostUrl, "");
     forderName = PROJECT_FOLDER_NAME + forderName.slice(0, forderName.lastIndexOf("/"));
     let fileName = forderName + URL.slice(URL.lastIndexOf("/")); // 파일명.확장자만 추출
@@ -73,13 +73,7 @@ async function filteringDownload(page, PROJECT_FOLDER_NAME, mainHostUrl, URLs) {
 
 async function createCssFile(page, PROJECT_FOLDER_NAME, mainHostUrl) {
   const mainPageCssURLs = await page.$$eval("link", (links) =>
-    links.map((link) => {
-      if (link.rel === "stylesheet") {
-        return link.href;
-      } else {
-        return false;
-      }
-    })
+    links.filter((link) => link.rel === "stylesheet").map((link) => link.href)
   );
   let nowUrlPath = await page.evaluate(() => {
     return window.location.href;
@@ -88,25 +82,8 @@ async function createCssFile(page, PROJECT_FOLDER_NAME, mainHostUrl) {
   await filteringDownload(page, PROJECT_FOLDER_NAME, mainHostUrl, cssURLs);
 }
 
-async function pathFiltering(URL, mainHostUrl, nowUrlPath) {
-  if (URL[0] === "/") {
-    return mainHostUrl + URL;
-  } else if (URL.includes("../")) {
-    let path = nowUrlPath.slice(0, nowUrlPath.lastIndexOf("/")); // 파일명.확장자 없애기
-    let count = URL.split("../").length - 1; // ../ 몇번 들어갔는지 카운트
-    for (let i = 0; i < count; i++) {
-      path = path.slice(0, path.lastIndexOf("/"));
-    }
-    path = `${path}/${URL}`;
-    return path.replace("../", "");
-  } else {
-    let path = nowUrlPath.slice(0, nowUrlPath.lastIndexOf("/") + 1);
-    return `${path + URL}`;
-  }
-}
-
 async function contentsFilteringURL(page, URLs, mainHostUrl, nowUrlPath) {
-  let data = [];
+  let data = [...URLs]; // css main 페이지가 배열로 들어옴, css main 페이지도 목록에 추가
   for await (const URL of URLs) {
     await page.goto(URL);
     await page.waitForTimeout(1000);
@@ -123,6 +100,26 @@ async function contentsFilteringURL(page, URLs, mainHostUrl, nowUrlPath) {
   return data;
 }
 
+function pathFiltering(URL, mainHostUrl, nowUrlPath) {
+  if (URL[0] === "/") {
+    return mainHostUrl + URL;
+  } else if (URL.includes("../")) {
+    let path = nowUrlPath.slice(0, nowUrlPath.lastIndexOf("/")); // 파일명.확장자 없애기
+    let count = URL.split("../").length - 1; // ../ 몇번 들어갔는지 카운트
+    for (let i = 0; i < count; i++) {
+      path = path.slice(0, path.lastIndexOf("/"));
+    }
+    path = `${path}/${URL}`;
+    return path.replace(/\.\.\//g, "");
+  } else {
+    let path = nowUrlPath.slice(0, nowUrlPath.lastIndexOf("/") + 1);
+    if (URL.includes("./")) {
+      path.replace("./", "");
+    }
+    return `${path + URL}`;
+  }
+}
+
 async function download(uri, filename, callback) {
   request.head(uri, function (err, res, body) {
     request(uri).pipe(fs.createWriteStream(filename)).on("close", callback);
@@ -134,7 +131,6 @@ async function createFolder(folderName) {
     // uploads 폴더 없으면 생성
     if (error) {
       fs.mkdirSync(folderName, { recursive: true });
-      console.log("폴더 생성 성공");
     }
   });
 }
